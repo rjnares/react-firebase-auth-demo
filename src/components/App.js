ea9caf21705552180143d1c92660f9fecba6309a
rjnares <roberto.nares@gmail.com>
2020-12-02 12:43:37 -0800

    Created ForgotPassword.js and UpdateProfile.js (both not implemented yet). Added link to UpdateProfile from Dashboard and link to ForgotPassword from Login.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import Dashboard from "./Dashboard";
 import Signup from "./Signup";
 import Login from "./Login";
+import UpdateProfile from "./UpdateProfile";
+import ForgotPassword from "./ForgotPassword";
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "../contexts/AuthContext";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -19,6 +21,8 @@ function App() {
               <PrivateRoute exact path="/" component={Dashboard} />
               <Route path="/signup" component={Signup} />
               <Route path="/login" component={Login} />
+              <Route path="/update-profile" component={UpdateProfile} />
+              <Route path="/forgot-password" component={ForgotPassword} />
             </Switch>
           </AuthProvider>
         </Router>
