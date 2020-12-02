609737f2ff53a3169b88411de896b9c78d271485
rjnares <roberto.nares@gmail.com>
2020-12-02 13:49:18 -0800

    Added update functions for email/password to AuthContext.js. Copied Signup.js code to UpdateProfile.js and replaced signup logic with update profile code that uses updateEmail/updatePassword functions from AuthContext.js.

--- src/components/UpdateProfile.js
+++ src/components/UpdateProfile.js
@@ -1,5 +1,92 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { Button, Card, Form, Alert } from "react-bootstrap";
+import { useAuth } from "../contexts/AuthContext";
+import { Link, useHistory } from "react-router-dom";
 
 export default function UpdateProfile() {
-  return <div>Update Profile</div>;
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmationRef = useRef();
+  const { currentUser, updatePassword, updateEmail } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (passwordRef.current.value !== passwordConfirmationRef.current.value) {
+      return setError("Passwords do not match");
+    }
+
+    const promises = [];
+    setError("");
+    setLoading(true);
+
+    if (emailRef.current.value !== currentUser.email) {
+      promises.push(updateEmail(emailRef.current.value));
+    }
+
+    if (passwordRef.current.value) {
+      promises.push(updatePassword(passwordRef.current.value));
+    }
+
+    Promise.all(promises)
+      .then(() => {
+        history.push("/");
+      })
+      .catch(() => {
+        setError("Failed to update profile");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Update Profile</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                ref={emailRef}
+                placeholder="Enter email"
+                required
+                defaultValue={currentUser.email}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                ref={passwordRef}
+                placeholder="Leave blank to keep the same password"
+              />
+            </Form.Group>
+
+            <Form.Group controlId="password-confirmation">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control
+                type="password"
+                ref={passwordConfirmationRef}
+                placeholder="Leave blank to keep the same password"
+              />
+            </Form.Group>
+
+            <Button disabled={loading} className="w-100" type="submit">
+              Update
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        <Link to="/">Cancel</Link>
+      </div>
+    </>
+  );
 }
